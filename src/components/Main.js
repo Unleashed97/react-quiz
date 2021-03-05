38541ff8ef1d5058c615fe4b468b1b2874d64fce
Nichkov Roman <nichkovroman@gmail.com>
2021-03-05 23:31:25 +0300

    add flags image, burger round

--- src/components/Main.js
+++ src/components/Main.js
@@ -6,12 +6,37 @@ const Main = () => {
             <section className="section">
                 <div className="container">
                     <div className="section__inner">
-                        <h1 className="section__title">Quizzes</h1>
+                        <h1 className="section__title">Geography quizzes</h1>
                         <div className="section__content">
                             {/* Cards */}
                             <div className="cards-list">
                                 <a href="/#" className="card">
-                                    <div className="card__title">Europe</div>
+                                    <div className="card__mask"></div>
+                                    <p className="card__title">
+                                        Capitals <br/>
+                                        <span>- of -</span> <br/>
+                                        Europe</p>
+                                </a>
+                                <a href="/#" className="card">
+                                <p className="card__title">
+                                        Capitals <br/>
+                                        <span>- of -</span> <br/>
+                                        Asia</p>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section className="section">
+                <div className="container">
+                    <div className="section__inner">
+                        <h1 className="section__title">Another quizzes</h1>
+                        <div className="section__content">
+                            {/* Cards */}
+                            <div className="cards-list">
+                                <a href="/#" className="card">
+                                    <div className="card__title">Something</div>
                                 </a>
                             </div>
                         </div>
