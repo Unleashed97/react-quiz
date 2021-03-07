515135cb12534fa12b6fade8495cd41bc3fd310e
Nichkov Roman <nichkovroman@gmail.com>
2021-03-07 14:55:38 +0300

    refactoring: removed old version

--- src/App.js
+++ src/App.js
@@ -13,36 +13,6 @@ import Home from './pages/Home';
 import Quiz from './pages/Quiz';
 
 function App() {
-
-  // const [currentAnswer, setCurrentAnswer] = useState(0);
-
-  // const [showScore, setShowScore] = useState(false);
-
-  // const [score, setScore] = useState(0);
-
-  // const [toggleMode, setToggleMode] = useState(false);
-
-  // const handleAnswerBtnClick = (isCorrect) =>{
-  //   if(isCorrect === true){
-  //     setScore(score + 1);
-  //   }
-  //   let nextAnswer = currentAnswer + 1;
-  //   if(nextAnswer < quiz.length){
-  //     setCurrentAnswer(nextAnswer);
-  //   }
-  //   else setShowScore(true);
-  // }
-
-  // const handleRepeatQuiz = () =>{
-  //   setShowScore(false);
-  //   setScore(0);
-  //   setCurrentAnswer(0);
-  // }
-  
-  // const handleCheckboxClick =() =>{
-  //   setToggleMode(!toggleMode);
-  // }
-
   return (
     <div className="wrapper">
       <HashRouter basename="/">
@@ -51,61 +21,6 @@ function App() {
           <Route exact path="/" component={Home} />
           <Route exact path="/quiz" component={Quiz} />
       </HashRouter>
-
-      {/* <div className="quiz">
-        {
-          showScore ? (
-            <div className="quiz__score">
-              <p className="quiz__score-text">You scored {score} out of {quiz.length}</p>
-              <div className="quiz__score-btns">
-                <button className="btn" onClick={handleRepeatQuiz} >Again</button>
-                <button className="btn">Another one</button>
-              </div>
-            </div>
-          ) :
-          (
-            <div className="quiz__inner">
-              <h1 className="quiz__title">Europe Capitals</h1>
-              <div className="quiz__content">
-                <div className="quiz__question">
-                  <p className="quiz__question-count"><span>Question {currentAnswer + 1}</span>/{quiz.length}</p>
-                  <p className="quiz__question-text">{quiz[currentAnswer].questionText}</p>
-                  <div className="quiz__control">
-                    <label className="check" htmlFor="check-input">
-                      Hard Mode
-                      <input id="check-input" className="check__input" type="checkbox" onChange={handleCheckboxClick} checked={toggleMode}/>
-                      <span className="check__slider"></span>
-                    </label>
-                  </div>
-                </div>
-                <div className="quiz__answer">
-                {
-                  toggleMode ? (
-                      <form className="form">
-                        <label className="form__label" htmlFor="form-input">
-                          <p className="form__label-text">Type the name of the capital here:</p>
-                          <input id="form-input" type="text" className="form__input" autoComplete="off"/>
-                        </label>
-                      </form>
-                  ) :
-                  (
-                    <div>
-                      {
-                        quiz[currentAnswer].answerOptions.map((answerOption, index) => (
-                            <button className="btn" onClick={()=>handleAnswerBtnClick(answerOption.isCorrect)} key={index}>{answerOption.answerText}</button>
-                        ))
-                      }
-                    </div>
-                  )
-                }
-                </div>
-              </div>
-            </div>
-          )
-        }
-
-      </div> */}
-
     </div>
   );
 }
