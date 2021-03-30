c65d1df4e9a427c94618b6fe0f8202d22025bd5e
Nichkov Roman <nichkovroman@gmail.com>
2021-03-30 11:31:48 +0300

    score block layout fix

--- src/components/QuizScore.js
+++ src/components/QuizScore.js
@@ -4,11 +4,13 @@ import quiz from '../data/quiz';
 
 const QuizScore = ({score, handleCheckAnswersBtnClick, handleTryAgainbtnClick}) => {
     return (
-        <div className="quiz__score">
-            <p className="quiz__score-text">You scored {score} out of {quiz.length}</p>
-            <div className="quiz__score-btns">
-                <button className="btn btn--score" onClick={handleCheckAnswersBtnClick}>Check answers</button>
-                <button className="btn btn--score" onClick={handleTryAgainbtnClick}>Try again</button>
+        <div className="score">
+            <div className="score__inner">
+                <p className="score__text">You scored {score} out of {quiz.length}</p>
+                <div className="score__btns">
+                    <button className="btn btn--score" onClick={handleCheckAnswersBtnClick}>Check answers</button>
+                    <button className="btn btn--score" onClick={handleTryAgainbtnClick}>Try again</button>
+                </div>
             </div>
         </div>
     )
